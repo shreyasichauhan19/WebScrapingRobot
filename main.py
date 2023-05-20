c879c40bec0271a87569e86388a4cef6799b9997
Shreyasi Chauhan <chauhan.shreyasi19@gmail.com>
2023-05-19 23:32:25 -0700

    Opens browser to the scientist's wikis

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from robotics import Robot
+from selenium import webdriver
+import time
 
+def open_wikipedia_pages(titles):
+    # Instantiate the web driver
+    driver = webdriver.Chrome()
+
+    # Iterate over the list of scientist names
+    for title in titles:
+        # Create the Wikipedia URL
+        url = 'https://en.wikipedia.org/wiki/' + title.replace(' ', '_')
+
+        # Open the URL in a new tab
+        driver.get(url)
+
+        # wait time before opening the next page
+        time.sleep(2)
+
+    # Wait for the user to hit Enter before ending the script, without this the driver just shuts down automatically
+    input("Press Enter to close the browser...")
+
+    driver.quit()
+
+# List of scientist names
 SCIENTISTS = ["Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin"]
 
-robot = Robot("Quandrinaut")
-
-
-def introduce_yourself():
-    robot.say_hello()
-
-
-def main():
-    introduce_yourself()
-
-
-if __name__ == "__main__":
-    main()
+# Call the function with the list of names
+open_wikipedia_pages(SCIENTISTS)
--- robotics.py
+++ robotics.py
@@ -1,18 +1,1 @@
-from RPA.Browser.Selenium import Selenium
-
-
-br = Selenium()
-
-
-class Robot:
-    def __init__(self, name):
-        self.name = name
-
-    def say_hello(self):
-        print("Hello, my name is " + self.name)
-
-    def say_goodbye(self):
-        print("Goodbye, my name is " + self.name)
-
-    def open_webpage(self, webpage):
-        br.open_available_browser(webpage)
+## code removed for now, can be added back when refactoring as needed.
