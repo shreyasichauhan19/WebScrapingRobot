600ca47c02e5ec559a248cd0be03211279eda8b0
Shreyasi Chauhan <chauhan.shreyasi19@gmail.com>
2023-05-20 22:48:54 -0700

    correct formatting of birth and death dates, some more code cleanup

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def introduce_yourself():
 def open_articles():
     for scientist in SCIENTISTS:
         formatted_scientist = scientist.replace(" ", "_")
-        robot.print_first_paragraph(f"https://en.wikipedia.org/wiki/{formatted_scientist}")
-        robot.print_bday_dday(f"https://en.wikipedia.org/wiki/{formatted_scientist}")
+        robot.data_scraping(f"https://en.wikipedia.org/wiki/{formatted_scientist}")
         print('-' * 50)
 
 
--- robotics.py
+++ robotics.py
@@ -2,6 +2,7 @@ from RPA.Browser.Selenium import Selenium
 from bs4 import BeautifulSoup
 import time
 import requests
+import re
 
 br = Selenium()
 
@@ -19,25 +20,25 @@ class Robot:
     def open_webpage(self, webpage):
         br.open_available_browser(webpage)
 
-    def print_first_paragraph(self, webpage):
+    def data_scraping(self, webpage):
         self.open_webpage(webpage)
-        time.sleep(5)  # Allow the page to load
+        time.sleep(2)  # Allow the page to load
         response = requests.get(webpage)
         soup = BeautifulSoup(response.text, 'html.parser')
         first_paragraph = soup.select_one('div.mw-parser-output > p:not(.mw-empty-elt)').text
-        print(first_paragraph)
-
-    def print_bday_dday(self, webpage):
-        self.open_webpage(webpage)
-        time.sleep(5)  # Allow the page to load
-        response = requests.get(webpage)
-        soup = BeautifulSoup(response.text, 'html.parser')
         date_of_birth = soup.find('span', {'class': 'bday'}).text if soup.find('span', {'class': 'bday'}) else 'N/A'
-        date_of_death ='N/A'
+        date_of_death = 'N/A'
         infobox = soup.find('table', {'class': 'infobox biography vcard'})
         if infobox:
             th_elements = infobox.find_all('th')
             for th in th_elements:
                 if "died" in th.text.lower():
                     date_of_death = th.find_next('td').text.strip().split('\n')[0].split('[')[0]
-        print(f"Birth date: {date_of_birth}\n- Death date: {date_of_death}\n")
+                    death_data_string = date_of_death
+                    match = re.search(r'\((.*?)\)', death_data_string)
+                    if match:
+                        death_date = match.group(1)
+        print(first_paragraph)
+        print(f"Date of Birth: {date_of_birth}")
+        print(f"Date of Death: {death_date}")
+    
